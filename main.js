a08df5d896a625d272249abc17c7c9d444844e1e
Stephanie Guzman <sguzman247@gmail.com>
2022-06-04 13:32:17 -0500

    Update functions

--- main.js
+++ main.js
@@ -1,60 +1,59 @@
 var newButton = document.querySelector('.new-button');
 var saveButton = document.querySelector('.save-button');
 var lockButtons = document.querySelectorAll('.lock');
-var lockButtonsArray = Array.prototype.slice.call(lockButtons);
+// var lockButtonsArray = Array.prototype.slice.call(lockButtons);
 var hexCodes = document.querySelectorAll('.hex');
-var hexCodesArray = Array.prototype.slice.call(hexCodes);
+// var hexCodesArray = Array.prototype.slice.call(hexCodes);
 var boxList = document.querySelectorAll('.boxlist')
-var boxListArray = Array.prototype.slice.call(boxList);
-// var boxColor = document.getElementById('#boxList');
+// var boxListArray = Array.prototype.slice.call(boxList);
+// var boxColor = document.getElementById('#boxList').style.backgroundColor = color;
 
 var palette = new Palette();
 var color = new Color();
-// palette.generateRandomPalette();
+palette.generateRandomPalette();
 
 newButton.addEventListener("click", updateColors);
 // lockButton1.addEventListener("click",)
 function removeClass(element, className) {
   element.classList.remove(className);
-}
+};
 
 function addClass(element, className) {
   element.classList.add(className);
-}
+};
 
 function toggleLock(lockButton) {
+  color = new Color();
   if (lockButton.classList.contains('open')) {
     removeClass(lockButton, 'open');
     lockButton.innerText = '🔒';
+    color.locked = true;
   } else {
     addClass(lockButton, 'open');
     lockButton.innerText = '🔓';
+    color.locked = false;
   }
-}
+};
 
-for(var i = 0; i < lockButtonsArray.length; i++) {
-  const button = lockButtonsArray[i];
-  button.addEventListener('click', function() {
-    toggleLock(button);
-  })
-}
-
-function updateColors() {
-  palette.generateRandomPalette();
-  for (var i = 0; i < hexCodesArray.length; i++) {
-    if (color.locked === false) {
-      // boxColor = color
-    }
+function eventListenerLock() {}
+  for(var i = 0; i < lockButtons.length; i++) {
+    const button = lockButtons[i];
+    button.addEventListener('click', function(e) {
+      toggleLock(e.target);
+    })
   }
 };
 
 function updateColors() {
+  // color = new Color();
   palette.generateRandomPalette();
-  for (var i = 0; i < hexCodesArray.length; i++) {
-    if (color.locked === false) {
-        hexCode.innerText = palette.colors[0].hexCode;
+    for (var i = 0; i < hexCodes.length; i++) {
+      if (palette.colors[i]color.locked === false) {
+        hexCodes[i].innerText = palette.colors[i].hexCode;
     }
   }
-}
+    // eventListenerLock();
+};
 
-console.log(palette)
+updateColors();
+console.log(palette);
--- palette.js
+++ palette.js
@@ -15,7 +15,21 @@ class Palette {
 
   generateRandomPalette() {
     for (var i = 0; i < 5; i++) {
-      this.generateColor(undefined, i);
+      this.generateColor(color.hex, i);
+    }
+  }
+
+  lockUnlockColor() {
+    // newColor = new Color();
+    for (var i = 0; i < palette.colors.length; i++) {
+      if (palette.colors[i].locked === false) {
+        palette.colors[i].locked = true;
+        lockButton.innerText = '🔒';
+        console.log(palette.colors, "colors.length");
+      } else {
+        palette.colors[i].locked = false;
+        lockButton.innerText = '🔓';
+      }
     }
   }
 }
