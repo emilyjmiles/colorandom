712b1ab251ff8d4bc7c035b377f9ede178f34211
Ana Bennett <anabennett1111@gmail.com>
2022-06-05 14:38:19 -0500

    complete iteration 4 and add functionality to the locks

--- palette.js
+++ palette.js
@@ -1,10 +1,21 @@
 class Palette {
-  constructor() {
+  constructor(colors) {
     this.id = Date.now();
-    this.colors = this.generateRandomPalette();
+    this.colors = colors || this.createRandomPalette();
   }
 
   generateRandomPalette() {
+      var randomPalette = [];
+      for (var i = 0; i < 5; i++) {
+        if(this.colors[i].locked === false) {
+          randomPalette.push(new Color());
+        } else {
+          randomPalette.push(this.colors[i]);
+        }
+      }
+      return randomPalette;
+  }
+  createRandomPalette() {
       var randomPalette = [];
       for (var i = 0; i < 5; i++) {
         randomPalette.push(new Color());
--- startFresh.js
+++ startFresh.js
@@ -1,6 +1,6 @@
 var newPaletteButton = document.querySelector('.new-button');
 var savePaletteButton = document.querySelector('.save-button');
-var lockButton = document.querySelectorAll('.lock')
+var lockButton = document.querySelectorAll('.lock');
 var lockButton1 = document.querySelector('.lock1');
 var lockButton2 = document.querySelector('.lock2');
 var lockButton3 = document.querySelector('.lock3');
@@ -8,35 +8,50 @@ var lockButton4 = document.querySelector('.lock4');
 var lockButton5 = document.querySelector('.lock5');
 var hexCodes = document.querySelectorAll('.hex');
 var boxList = document.querySelectorAll('.boxlist');
-var allColorBoxes = document.querySelector('.all-color-boxes')
+var allColorBoxes = document.querySelector('.all-color-boxes');
+var savedColors = document.querySelector('.saved-colors');
 
 var currentPalette = "";
+var savedPalettes = [];
 
-//event listener for lock/unlock color
 
-// lockButton1.addEventListener("click",palette.lockUnlockColor);
-// lockButton2.addEventListener("click",palette.lockUnlockColor);
-// lockButton3.addEventListener("click",palette.lockUnlockColor);
-// lockButton4.addEventListener("click",palette.lockUnlockColor);
-// lockButton5.addEventListener("click",palette.lockUnlockColor);
+
+
 window.addEventListener("load", onPageLoad)
 newPaletteButton.addEventListener("click", createNewPalette)
+savePaletteButton.addEventListener("click", saveMiniPalette)
+allColorBoxes.addEventListener("click", lockColor)
 
 
 function onPageLoad() {
-    currentPalette = new Palette;
-    updateColors();//displaying what our palette is displaying
+    currentPalette = new Palette();
+    updateColors();
+}
+function saveMiniPalette() {
+  saveCurrentPalette();
+  displayMiniPalette();
+  createNewPalette();
 }
 
 function updateColors() {
+  var lockedEmoji = "";
+  var lockedClass = "";
   var allColors = "";
-  for(var i = 0; i < currentPalette.colors.length; i++) {
-    allColors += `<section class="individual-box">
-      <section class="box1" style="background-color:${currentPalette.colors[i].hexCode}" id="boxlist">
+  for (var i = 0; i < currentPalette.colors.length; i++) {
+    if(currentPalette.colors[i].locked === true) {
+      lockedEmoji = '🔒';
+      lockedClass = 'close';
+    } else {
+      lockedEmoji = '🔓';
+      lockedClass = 'open';
+    }
+    allColors +=
+    `<section class="individual-box">
+      <section class="box" style="background-color:${currentPalette.colors[i].hexCode}" id="boxlist${i}">
       </section>
       <div class="color-and-lock">
         <h2 class="hex">${currentPalette.colors[i].hexCode}</h2>
-        <h3 class="lock1 open" type="lock">🔓</h3>
+        <h3 class="lock ${lockedClass}" type="lock">${lockedEmoji}</h3>
       </div>
     </section>`
   }
@@ -46,4 +61,39 @@ function updateColors() {
 function createNewPalette() {
   currentPalette.colors = currentPalette.generateRandomPalette();
   updateColors();
+}
+
+function saveCurrentPalette() {
+  var savedPalette = new Palette(currentPalette.colors);
+  savedPalettes.push(savedPalette);
+}
+
+function displayMiniPalette() {
+  savedColors.innerHTML = "";
+  for (var i = 0; i < savedPalettes.length; i++) {
+    savedColors.innerHTML +=
+    `<section class="mini-palette">
+      <div class="mini-box" style="background-color: ${savedPalettes[i].colors[0].hexCode}"></div>
+      <div class="mini-box" style="background-color: ${savedPalettes[i].colors[1].hexCode}"></div>
+      <div class="mini-box" style="background-color: ${savedPalettes[i].colors[2].hexCode}"></div>
+      <div class="mini-box" style="background-color: ${savedPalettes[i].colors[3].hexCode}"></div>
+      <div class="mini-box" style="background-color: ${savedPalettes[i].colors[4].hexCode}"></div>
+      <div class="trash-can">🗑</div>
+    </section>`
+  }
+}
+
+  function lockColor(event) {
+    var colorboxarea = event.target.id.slice(0, 7);
+    if (colorboxarea === "boxlist") {
+      var index = event.target.id.slice(7, 8);
+      if(currentPalette.colors[index].locked === false) {
+         currentPalette.colors[index].locked = true;
+        } else {
+          currentPalette.colors[index].locked = false;
+        }
+
+        updateColors();
+
+  }
 }
