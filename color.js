ed6301c63633fa2e0b670764b6e4497fb890c56d
Emily Miles <ms.emily.j.miles@gmail.com>
2022-06-06 16:35:35 -0400

    Refactor javascript files

--- color.js
+++ color.js
@@ -1,19 +1,19 @@
 class Color {
-    constructor() {
-      this.hexCode = this.generateHexCode();
-      this.locked = false;
-    }
+  constructor() {
+    this.hexCode = this.generateHexCode();
+    this.locked = false;
+  }
 
-    randomNumber(array) {
-      return Math.floor(Math.random() * array.length);
-    }
+  randomNumber(array) {
+    return Math.floor(Math.random() * array.length);
+  }
 
-    generateHexCode() {
-      var hexChoices = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-      var hexId = '#';
-      for(var i = 0; i < 6; i++) {
-        hexId += hexChoices[this.randomNumber(hexChoices)];
+  generateHexCode() {
+    var hexChoices = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+    var hexId = '#';
+    for(var i = 0; i < 6; i++) {
+      hexId += hexChoices[this.randomNumber(hexChoices)];
     }
-      return hexId;
+    return hexId;
   }
 };
--- palette.js
+++ palette.js
@@ -5,22 +5,22 @@ class Palette {
   }
 
   generateRandomPalette() {
-      var randomPalette = [];
-      for (var i = 0; i < 5; i++) {
-        if(this.colors[i].locked === false) {
-          randomPalette.push(new Color());
-        } else {
-          randomPalette.push(this.colors[i]);
-        }
+    var randomPalette = [];
+    for (var i = 0; i < 5; i++) {
+      if(this.colors[i].locked === false) {
+        randomPalette.push(new Color());
+      } else {
+        randomPalette.push(this.colors[i]);
       }
+    }
     return randomPalette;
   }
 
   createRandomPalette() {
-      var randomPalette = [];
-      for (var i = 0; i < 5; i++) {
-        randomPalette.push(new Color());
+    var randomPalette = [];
+    for (var i = 0; i < 5; i++) {
+      randomPalette.push(new Color());
     }
     return randomPalette;
   }
-}
+};
