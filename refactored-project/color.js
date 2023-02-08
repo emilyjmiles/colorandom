e41cea37335a100c801d899959ecf0eccbfdd009
Emily Miles <ms.emily.j.miles@gmail.com>
2023-02-08 10:15:05 -0700

    Refactor variable types and remove unused html

--- refactored-project/color.js
+++ refactored-project/color.js
@@ -1,5 +1,5 @@
 class Color {
-  constructor() {
+  constructor () {
     this.hexCode = this.generateHexCode();
     this.locked = false;
   }
@@ -9,9 +9,9 @@ class Color {
   }
 
   generateHexCode() {
-    var hexChoices = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-    var hexId = '#';
-    for(var i = 0; i < 6; i++) {
+    const hexChoices = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    let hexId = '#';
+    for (let i = 0; i < 6; i++) {
       hexId += hexChoices[this.randomNumber(hexChoices)];
     }
     return hexId;
